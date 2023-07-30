3a5596bbcef3e593129bf87980987dd95054d4e9
Sufyan Ahmed <sufiyan033130@gmail.com>
2023-07-30 10:36:14 +0500

    changes route

--- src/components/CheckAllProducts.jsx
+++ src/components/CheckAllProducts.jsx
@@ -6,7 +6,7 @@ const CheckAllProducts = () => {
 
   useEffect(() => {
     axios
-      .get('https://real-pink-scallop-kit.cyclic.app/')
+      .get('https://real-pink-scallop-kit.cyclic.app/check')
       .then((response) => {
         setProducts(response.data);
       })
